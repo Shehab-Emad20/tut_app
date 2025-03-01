3eb37ea55dbc7e9bd1a79b0aceb83b7a642347e4
Shehab-Emad20 <shehabz2014@gmail.com>
2025-03-02 01:06:43 +0200

    Creating Styles Manager

---
+++ lib/presentation/styles_manger.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/font_manger.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: FontConstants.fontFamily,
+    fontWeight: fontWeight,
+    color: color,
+  );
+}
+
+TextStyle getRegularStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.regular,
+    color,
+  );
+} 
+
+TextStyle getMediumStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.medium,
+    color,
+  );
+}
+
+TextStyle getLightStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.light,
+    color,
+  );  
+}
+
+TextStyle getSemiBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.semiBold,
+    color,
+  );
+}
+
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.bold,
+    color,
+  );
+}
+
+
