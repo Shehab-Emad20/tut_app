611c52f1815dde405effe1dd57299d0786fd1338
Shehab-Emad20 <shehabz2014@gmail.com>
2025-03-01 23:59:53 +0200

    Creating Colors Manager

---
+++ lib/presentation/color_manger.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManger {
+  static Color primaryColor = Color(0xffED9728);
+  static Color darkGrey = Color(0xff525252);
+  static Color grey = Color(0xff737477);
+  static Color lightGrey = Color(0xff9E9E9E);
+}
