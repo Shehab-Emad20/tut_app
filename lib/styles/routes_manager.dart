01ead58b93c9bf647b7461b3126446da72a3bba5
Shehab-Emad20 <shehabz2014@gmail.com>
2025-03-02 14:06:23 +0200

    finish Adding Routes Manager

--- lib/styles/routes_manager.dart
+++ lib/styles/routes_manager.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/forgotpassword/forgot_password.dart';
+import 'package:tut_app/presentation/login/login_view.dart';
+import 'package:tut_app/presentation/main/main_view.dart';
+import 'package:tut_app/presentation/register/register_view.dart';
+import 'package:tut_app/presentation/splash/splash_view.dart';
+import 'package:tut_app/presentation/storedetails/store_details.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String loginRoute = "/login";
@@ -5,4 +13,41 @@ class Routes {
   static const String forgotpasswordRoute = "/forgotpassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+
+      case Routes.forgotpasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPassword());
+
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetails());
+
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder:
+          (_) => Scaffold(
+            appBar: AppBar(title: const Text("No Route Found")),
+            body: const Center(child: Text("No Route Found")),
+          ),
+    );
+  }
 }
